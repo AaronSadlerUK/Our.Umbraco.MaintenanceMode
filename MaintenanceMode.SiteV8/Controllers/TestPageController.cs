4bd17d62b9c51a0c172c99aceab9078a638142b6
Aaron Sadler <aaron@aaronsadler.co.uk>
2020-05-30 12:18:36 +0100

    Added a test render controller

---
+++ MaintenanceMode.SiteV8/Controllers/TestPageController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using MaintenanceMode.SiteV8.Models;
+using Umbraco.Web.Models;
+
+public class TestPageController : Umbraco.Web.Mvc.RenderMvcController
+{
+    // Any request for the 'ProductAmpPage' template will be handled by this Action
+    public ActionResult TestTemplatePage(ContentModel contentModel)
+    {
+        var model = new TestModel(contentModel.Content);
+        // Create AMP specific content here...
+        return CurrentTemplate(model);
+    }
+    // All other request, eg the ProductPage template will be handled by the default 'Index' action
+    public override ActionResult Index(ContentModel contentModel)
+    {
+        // you are in control here!
+
+        // return a 'model' to the selected template/view for this page.
+        return CurrentTemplate(contentModel);
+    }
+}
---
+++ MaintenanceMode.SiteV8/Models/TestModel.cs
@@ -0,0 +1,12 @@
+﻿using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Web.Models;
+
+namespace MaintenanceMode.SiteV8.Models
+{
+    public class TestModel : ContentModel
+    {
+        public TestModel(IPublishedContent content) : base(content)
+        {
+        }
+    }
+}
